12a5e76ee242d10d2f38b15dbb9c86212e08710b
ren0nmansoneves@gmail.com <ren0nmansoneves@gmail.com>
2025-07-23 21:41:23 -0300

    inicio do desenvolvimento do subheader

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,23 @@
+import { NavLink } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import { SubHeader } from "../SubHeader/SubHeader";
 
 export const Header = () => {
   const { user, logout } = useAuth();
   return (
-    <header className="dashboard-header">
-      <div className="header-content">
-        <h1>📚 Painel Administrativo - Biblioteca</h1>
-        <div className="user-info">
-          <span>Bem-vindo, {user?.name || user?.email}</span>
-          <button onClick={logout} className="logout-btn">
-            Sair
-          </button>
+    <>
+      <header className="dashboard-header">
+        <div className="header-content">
+          <h1>📚 Painel Administrativo - Biblioteca</h1>
+          <div className="user-info">
+            <span>Bem-vindo, {user?.name || user?.email}</span>
+            <button onClick={logout} className="logout-btn">
+              Sair
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <SubHeader />
+    </>
   );
 };
